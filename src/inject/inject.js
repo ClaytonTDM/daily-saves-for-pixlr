554cb3e908f1cbac9158fecc064e064efecf43ff
ClaytonTDM <claytontdm@gmail.com>
2023-07-06 21:00:48 -0400

    Upload source

---
+++ src/inject/inject.js
@@ -0,0 +1,3 @@
+window.addEventListener('load', function () {
+localStorage.setItem('user-settings', '{"lastNewsCheck":"1969-12-31T01:01:01.900Z","dailySave":{"day":1,"tally":1}}'); setTimeout(function(){document.getElementsByClassName('modal')[0].remove();},500);
+});
