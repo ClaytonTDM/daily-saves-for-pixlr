7e83d897be2668b34a4f82724dfdd17fc2b83a60
ClaytonTDM <claytontdm@gmail.com>
2024-02-04 07:04:44 -0500

    firefox version

---
+++ src/inject/inject.js
@@ -0,0 +1,5 @@
+setTimeout(function () { document.getElementsByClassName('modal')[0].remove(); }, 500);
+setInterval(() => {
+    console.log('resetting daily saves [do NOT dmca me i am NOT breaking any copyright laws]')
+    localStorage.setItem('user-settings', '{"lastNewsCheck":"1969-12-31T01:01:01.900Z","dailySave":{"day":1,"tally":1}}');
+}, 1000);
