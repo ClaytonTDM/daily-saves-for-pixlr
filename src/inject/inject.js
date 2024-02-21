a72d39e477df94bb7962eeb8993b0baa7a27cd22
ClaytonTDM <claytontdm@gmail.com>
2024-02-21 10:49:53 -0500

    Update reset logic (whats a ddid 😭)

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,7 +1,26 @@
+localStorage.setItem('user-settings', '{"lastNewsCheck":"1970-01-01T00:00:00Z"}');
+let deletedModal = false;
 window.addEventListener('load', function () {
-    setTimeout(function () { document.getElementsByClassName('modal')[0].remove(); }, 500);
+    /*
+    let targetNode = document.body;
+    let config = { childList: true, subtree: true };
+    let observer = new MutationObserver(function (mutationsList, observer) {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                let modal = document.getElementsByClassName('modal')[0];
+                if (modal && !deletedModal) {
+                    setTimeout(() => {
+                        modal.remove();
+                        deletedModal = true;
+                    }, 50);
+                }
+            }
+        }
+    });
+    observer.observe(targetNode, config);
+    */
     setInterval(() => {
-        console.log('resetting daily saves [do NOT dmca me i am NOT breaking any copyright laws]')
-        localStorage.setItem('user-settings', '{"lastNewsCheck":"1969-12-31T01:01:01.900Z","dailySave":{"day":1,"tally":1}}');
-    }, 1000);
+        console.log('resetting daily saves')
+        localStorage.setItem('user-settings', `{"lastNewsCheck":"${new Date().toISOString()}"}`);
+    }, 500);
 });
