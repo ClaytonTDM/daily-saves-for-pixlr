0136d8f717ffdffd0109c39bbaa56fced8f079c6
ClaytonTDM <claytontdm@gmail.com>
2024-09-21 21:04:13 -0400

    Update inject.js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,24 +1,26 @@
 localStorage.setItem('user-settings', '{"lastNewsCheck":"1970-01-01T00:00:00Z"}');
 let deletedModal = false;
-/*
-let targetNode = document.body;
-let config = { childList: true, subtree: true };
-let observer = new MutationObserver(function (mutationsList, observer) {
-    for (let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            let modal = document.getElementsByClassName('modal')[0];
-            if (modal && !deletedModal) {
-                setTimeout(() => {
-                    modal.remove();
-                    deletedModal = true;
-                }, 50);
+window.addEventListener('load', function () {
+    /*
+    let targetNode = document.body;
+    let config = { childList: true, subtree: true };
+    let observer = new MutationObserver(function (mutationsList, observer) {
+        for (let mutation of mutationsList) {
+            if (mutation.type === 'childList') {
+                let modal = document.getElementsByClassName('modal')[0];
+                if (modal && !deletedModal) {
+                    setTimeout(() => {
+                        modal.remove();
+                        deletedModal = true;
+                    }, 50);
+                }
             }
         }
-    }
+    });
+    observer.observe(targetNode, config);
+    */
+    setInterval(() => {
+        console.log('resetting daily saves')
+        localStorage.setItem('user-settings', `{"lastNewsCheck":"${new Date().toISOString()}"}`);
+    }, 500);
 });
-observer.observe(targetNode, config);
-*/
-setInterval(() => {
-    console.log('resetting daily saves')
-    localStorage.setItem('user-settings', `{"lastNewsCheck":"${new Date().toISOString()}"}`);
-}, 500);
